6054054be29df9fc2b1b429ce7605968cad14d4f
Sabin Manasia <manasiasabinpaltin@gmail.com>
2022-03-17 12:20:32 +0000

    remote repo clean-up

---
+++ src/hompage.component.jsx
@@ -0,0 +1,40 @@
+import './homepage.style.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classNam='title'>HATS</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classNam='title'>Jackets</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classNam='title'>Sneakers</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classNam='title'>Womens</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 classNam='title'>Mens</h1>
+                    <span className='subtitle'>Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
